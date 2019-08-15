2e46a49432be17dfd43cd471a95060fe7d92ed4a
Cristopher Palacios <criszelaya24@hotmail.com>
2019-08-15 15:51:21 -0600

    using map and reduce function to concat the object of element into the array

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -2,13 +2,16 @@ import React from 'react'
 import classes from './Burger.css'
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 const burger = (props) => {
-    // TRansforming object to array
+    // Transforming object to array
     const transformedIngredients = Object.keys(props.ingredients)
         .map(igKey => {
             return [...Array(props.ingredients[igKey])].map((_, i) => {
                return <BurgerIngredient key={igKey + i} type= {igKey}/>;
             });
-        });
+        }).reduce((arr,el) =>{
+            return arr.concat(el)
+        }, []);
+
     return(
         <div className={classes.Burger}>
             <BurgerIngredient type='bread-top'/>

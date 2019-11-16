e5f032b99e189db1606ba3264244e7153dc476b7
Cristopher Palacios <criszelaya24@hotmail.com>
2019-11-16 16:25:28 -0300

    Open and closing sideDrawer (180)

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -4,15 +4,18 @@ import Toolbar from '../Navigation/Toolbar/Toolbar'
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
 class Layout extends Component {
     state ={
-        showSideDrawer: true
+        showSideDrawer: false
     }
     sideDrawerCloseHandler = () => {
         this.setState({showSideDrawer: false});
     }
+    sideDrawerOpenHandler = () => {
+        this.setState({showSideDrawer: true})
+    }
     render(){
         return(
             <div>
-                <Toolbar/>
+                <Toolbar openSideDrawer = {this.sideDrawerOpenHandler}/>
                 <SideDrawer open={this.state.showSideDrawer} closed = {this.sideDrawerCloseHandler}/>
                 <main className = {classes.Content}>
                     {this.props.children}

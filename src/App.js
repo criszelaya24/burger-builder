edab59b2fd5318e284ac535f4adf4d27fc4e55f8
Cristopher Palacios <criszelaya24@hotmail.com>
2019-11-16 15:06:44 -0300

    Renaming folder layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Layout from './components/Layouts/Layouts'
+import Layout from './components/Layout/Layout'
 import BurgerBuilder from './containers/BurguerBuilder/BurgerBuilder'
 function App() {
   return (

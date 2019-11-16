6314465289b7ef3f4112dff113c87b076f51dfc2
Cristopher Palacios <criszelaya24@hotmail.com>
2019-11-16 16:13:52 -0300

    Playing with the sidedrawer and the backdrop (179)

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, {Component} from 'react';
 import classes from './Layout.css'
 import Toolbar from '../Navigation/Toolbar/Toolbar'
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
-const layout = (props) => (
-    <div>
-        <Toolbar/>
-        <SideDrawer/>
-        <main className = {classes.Content}>
-        {props.children}
-        </main>
-    </div>
-);
-
-export default layout
+class Layout extends Component {
+    state ={
+        showSideDrawer: true
+    }
+    sideDrawerCloseHandler = () => {
+        this.setState({showSideDrawer: false});
+    }
+    render(){
+        return(
+            <div>
+                <Toolbar/>
+                <SideDrawer open={this.state.showSideDrawer} closed = {this.sideDrawerCloseHandler}/>
+                <main className = {classes.Content}>
+                    {this.props.children}
+                </main>
+            </div>
+        )
+    }
+}
+export default Layout
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,11 +2,19 @@ import React from 'react'
 import Logo from '../../Logo/Logo'
 import NavigationItems from '../NavigationItems/NavigationItems'
 import classes from './SideDrawer.css'
-const sideDrawer = (props) => (
-    <div className={classes.SideDrawer}>
-        <Logo height="11%" marginBottom="32px"/>
-        <nav><NavigationItems/></nav>
-    </div>
-)
+import Backdrop from '../../UI/Backdrop/Backdrop'
+const sideDrawer = (props) => {
+    let attachedClasses = []
+    props.open ? attachedClasses = [classes.SideDrawer, classes.Open] : attachedClasses = [classes.SideDrawer, classes.Close]
+    return(
+        <div>
+            <Backdrop show={props.open} clicked = {props.closed}/>
+            <div className={attachedClasses.join(' ')}>
+                <Logo height="11%" marginBottom="32px"/>
+                <nav><NavigationItems/></nav>
+            </div>
+        </div>
+    )
+}
 
 export default sideDrawer;

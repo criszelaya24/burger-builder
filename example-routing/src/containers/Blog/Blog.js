c44ebb70bb77f71fd14c7f57c47b6461dc4ac62d
Cristopher Palacios <criszelaya24@hotmail.com>
2020-01-28 17:11:59 -0300

    using switch, what is does is that bring a unique component at time

--- example-routing/src/containers/Blog/Blog.js
+++ example-routing/src/containers/Blog/Blog.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, NavLink } from 'react-router-dom'
+import { Route, NavLink, Switch } from 'react-router-dom'
 import Posts from './Posts/Posts'
 import NewPost from './NewPost/NewPost'
 import FullPost from './FullPost/FullPost'
@@ -18,9 +18,11 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-                <Route path='/' exact component={Posts}/>
-                <Route path='/new-post' exact component={NewPost}/>
-                <Route path='/posts/:postId' exact component={FullPost}/>
+                <Switch>
+                    <Route path='/' exact component={Posts}/>
+                    <Route path='/new-post' exact component={NewPost}/>
+                    <Route path='/posts/:postId' exact component={FullPost}/>
+                </Switch>
             </div>
         );
     }

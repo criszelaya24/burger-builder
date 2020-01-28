ee0bee30e403de2d8eae688cbc1237ac5bc6eafa
Cristopher Palacios <criszelaya24@hotmail.com>
2020-01-28 14:23:42 -0300

    Using Route component and exact private word to render component

--- example-routing/src/containers/Blog/Blog.js
+++ example-routing/src/containers/Blog/Blog.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-// import axios from 'axios';
-import axios from '../../axios';
+import { Route } from 'react-router-dom'
 import Posts from './Posts/Posts'
 import './Blog.css';
 
@@ -17,7 +16,7 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-                <Posts/>
+                <Route path='/' exact component={Posts}/>
             </div>
         );
     }

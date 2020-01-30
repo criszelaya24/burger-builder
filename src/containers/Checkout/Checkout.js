14e72c79a8a8004a3b1e615f93b509a6a418a3b1
Cristopher Palacios <criszelaya24@hotmail.com>
2020-01-30 18:25:16 -0300

    handling clicks and redirect user after click

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -9,10 +9,21 @@ class Checkout extends Component {
             bacon: 1
         }
     }
+
+    checkoutCancel = () => {
+        this.props.history.goBack()
+    }
+
+    checkoutContinue = () => {
+        this.props.history.replace('/checkout/contact-data')
+    }
     render(){
         return(
             <div>
-                <CheckoutSummary ingredients={this.state.ingredients}/>
+                <CheckoutSummary
+                    ingredients={this.state.ingredients}
+                    checkoutCancel={this.checkoutCancel}
+                    checkoutContinue={this.checkoutContinue}/>
             </div>
         )
     }

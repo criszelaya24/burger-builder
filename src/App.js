d42255d9001d8bf8762eb9376140b98fbf7f1c70
Cristopher Palacios <criszelaya24@hotmail.com>
2020-01-30 17:55:43 -0300

    Using Routing, BrowserRouter, Router and Switch

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import Layout from './containers/Layout/Layout'
 import BurgerBuilder from './containers/BurguerBuilder/BurgerBuilder'
 import Checkout from './containers/Checkout/Checkout'
+import { Route, Switch } from 'react-router-dom'
 function App() {
   return (
     <div>
       <Layout children = 'Test from children'>
-        <BurgerBuilder />
-        <Checkout/>
+        <Switch>
+          <Route path="/checkout" exact component={Checkout}/>
+          <Route path="/" exact component={BurgerBuilder}/>
+        </Switch>
       </Layout>
     </div>
   );

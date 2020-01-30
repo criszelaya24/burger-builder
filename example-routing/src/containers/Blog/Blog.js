987f7f1f7c30456e7e19785737ff89f2ffa089b5
Cristopher Palacios <criszelaya24@hotmail.com>
2020-01-30 14:57:16 -0300

    How to load component when you need it, Lazy routes!

--- example-routing/src/containers/Blog/Blog.js
+++ example-routing/src/containers/Blog/Blog.js
@@ -1,24 +1,28 @@
 import React, { Component } from 'react';
 import { Route, NavLink, Switch, Redirect } from 'react-router-dom'
 import Posts from './Posts/Posts'
-import NewPost from './NewPost/NewPost'
+// import NewPost from './NewPost/NewPost'
 import FullPost from './FullPost/FullPost'
+import asyncComponent from '../../hoc/asyncComponent'
 import './Blog.css';
 
+const AsyncComponent = asyncComponent(() => {
+    return import('./NewPost/NewPost');
+})
 class Blog extends Component {
 
     state = {
-        auth: false
+        auth: true
     }
 
     render () {
-        let newPostRoute = this.state.auth ? <Route path='/new-post' exact component={NewPost}/> : null
+        let newPostRoute = this.state.auth ? <Route path='/new-post' exact component={AsyncComponent}/> : null
         return (
             <div className= "Blog">
                 <header>
                     <nav>
                         <ul>
-                            <li><NavLink exact to='/'>Posts</NavLink></li>
+                            <li><NavLink exact to='/posts'>Posts</NavLink></li>
                             <li><NavLink exact to='/new-post'>New Post</NavLink></li>
                         </ul>
                     </nav>
@@ -27,7 +31,8 @@ class Blog extends Component {
                     { newPostRoute }
                     <Route path='/posts' exact component={Posts}/>
                     <Route path='/posts/:postId' exact component={FullPost}/>
-                    <Redirect from="/" to="/posts"/>
+                    <Route render={() => <h1>Not found</h1> /* Handle 404 request*/ }/>
+                    {/*<Redirect from="/" to="/posts"/> Using redirect*/}
                 </Switch>
             </div>
         );

7691625e6c088d494042cd59713aa2f2cafe24ed
Cristopher Palacios <criszelaya24@hotmail.com>
2020-01-30 17:40:08 -0300

    Creating the checkout summary to use router after

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Layout from './containers/Layout/Layout'
 import BurgerBuilder from './containers/BurguerBuilder/BurgerBuilder'
+import Checkout from './containers/Checkout/Checkout'
 function App() {
   return (
     <div>
       <Layout children = 'Test from children'>
         <BurgerBuilder />
+        <Checkout/>
       </Layout>
     </div>
   );

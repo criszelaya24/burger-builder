bebb25a011dbfafb8f1ae0d5dce6e6ce266a85b8
cristopher Palacios <cristopher@fanatiz.com>
2020-03-06 16:46:54 -0300

    retrieving orders and looping into each other

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -1,10 +1,22 @@
 import React from 'react'
 import Classes from './Order.css'
 const order = (props) => {
+    const ingredients = []
+    Object.entries(props.ingredients).forEach(([key, value])=>{
+        ingredients.push({ name: key, amount: value })
+    })
+    const ingredientsOutputs = ingredients.map((ingredient, i) => {
+        return <span style={{textTransform: 'capitalize',
+                             display: 'inline-block',
+                             margin: '0 8px',
+                             border: '1px solid #ccc',
+                             padding: '5px'}}
+                     key={i}>{ingredient.name}: ({ ingredient.amount })</span>
+    })
     return(
         <div className={Classes.Order}>
-            <p>Ingredients: </p>
-            <p>Price: <strong>USD</strong></p>
+            {ingredientsOutputs}
+            <p>Price: <strong>USD { props.price.toFixed(2)}</strong></p>
         </div>
     )
 }
--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,13 +1,42 @@
 import React, { Component } from "react";
 import Order from '../../components/Order/Order'
+import axios from '../../axios-orders'
 
 class Orders extends Component {
-
+    state = {
+        orders: [],
+        loading: false,
+        errorMessage: null
+    }
+    componentDidMount(){
+        axios.get('/orders.json')
+        .then(res => {
+            let ordersData = [];
+            Object.entries(res.data).forEach(([key, value])=>{
+                ordersData.push({
+                    ...value,
+                    id: key
+                })
+            })
+            this.setState({
+                orders: ordersData,
+                loading: false
+            })
+        })
+        .catch( err => {
+            this.setState({loading: false, errorMessage: err.message})
+        })
+    }
     render(){
+        console.log(this.state.orders)
+        let Orders = null
+        Orders = this.state.orders.map(order => {
+            return <Order key={order.id} price={+order.price} ingredients={order.ingredients}/>
+        })
+        if (this.state.errorMessage !== null) Orders = <div>{ this.state.errorMessage }</div>;
         return(
             <div>
-                <Order/>
-                <Order/>
+                { Orders }
             </div>
         )
     }
